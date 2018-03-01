74fcd66572cff40d4f30d32fc5e3b733814e8892
Qinyifan <yifanq@kth.se>
2018-02-28 22:17:58 -0800

    Mod: add router 403 and entrance. access control unfinished on '/'

--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,34 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Entrance from '@/components/Entrance'
-Vue.use(Router)
-
-export default new Router({
+import Forbidden from '@/components/Forbidden'
+Vue.use(Router);
+const router = new Router({
     routes: [
         {
             path: '/',
             name: 'HelloWorld',
-            component: HelloWorld
+            component: HelloWorld,
+            beforeEnter: (to, from, next) => {
+                console.log('to:',to);
+                console.log('from:',from);
+                next();
+                console.log('cookies:', Vue.prototype.$cookies.get('roomId')); // TODO validate cookie (token) in the server set
+                // cookie
+
+
+            }
         },
         {
-            path:'/entrance',
-            name:'Entrance',
-            component:Entrance
+            path: '/entrance',
+            name: 'Entrance',
+            component: Entrance
+        },
+        {
+            path: '/403',
+            name: 'Forbidden',
+            component: Forbidden
         }
     ]
-})
+});
+export default router;
