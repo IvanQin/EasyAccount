540eeaecec06dd1e14b0fa9e20183329b690173c
Qinyifan <yifanq@kth.se>
2018-03-14 00:33:03 -0700

    Mod: add access control to main part (unfinished)

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import HelloWorld from '@/components/HelloWorld'
 import Entrance from '@/components/Entrance'
 import Forbidden from '@/components/Forbidden'
 Vue.use(Router);
+const utils = require('../utils/utils');
 const router = new Router({
     routes: [
         {
@@ -11,12 +12,32 @@ const router = new Router({
             name: 'HelloWorld',
             component: HelloWorld,
             beforeEnter: (to, from, next) => {
-                console.log('to:',to);
-                console.log('from:',from);
-                next();
-                console.log('cookies:', Vue.prototype.$cookies.get('roomId')); // TODO validate cookie (token) in the server set
+                console.log('to:', to);
+                console.log('from:', from);
+                let accessToken = Vue.prototype.$cookies.get('access-token');
+                let roomId = Vue.prototype.$cookies.get('roomId');
+                let validateTokenRequest = utils.getDbOperationTemplate('auth', utils.SEARCH, {
+                    document: {
+                        token: accessToken,
+                        roomId: roomId
+                    }
+                });
+                console.log(validateTokenRequest);
+                Vue.prototype.$http.post('/api/validate-token', {dbDocument: validateTokenRequest}).then(res => {
+                    let receivedData = res.data;
+                    console.log(receivedData);
+                    if (receivedData == 'OK') {
+                        next();
+                    }
+                    else {
+                        next({
+                            name: 'Forbidden'
+                        });
+                    }
+                });
+                console.log('cookies:', Vue.prototype.$cookies.get('access-token')); // TODO validate cookie (token) in the server set
                 // cookie
-
+                // next();
 
             }
         },
