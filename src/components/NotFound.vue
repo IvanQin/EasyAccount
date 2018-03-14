01a3dc16da91b46fe811fca4594441adb8c77f3b
Qinyifan <yifanq@kth.se>
2018-03-14 11:13:30 -0700

    Add: add 404 page

---
+++ src/components/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="404">
+        <el-container>
+            <el-header>
+
+            </el-header>
+            <el-main>
+                <div class="center">
+
+                    404 Not Found.
+
+
+                </div>
+
+            </el-main>
+        </el-container>
+
+    </div>
+</template>
